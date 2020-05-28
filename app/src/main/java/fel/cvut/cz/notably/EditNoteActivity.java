4edb75ba92e629ce56c54442762025f7ae6f3f00
Patrik Sedlisky <patriksed23@gmail.com>
2020-05-28 14:32:28 +0200

    Edit note activity

---
+++ app/src/main/java/fel/cvut/cz/notably/EditNoteActivity.java
@@ -0,0 +1,13 @@
+package fel.cvut.cz.notably;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+    }
+}
